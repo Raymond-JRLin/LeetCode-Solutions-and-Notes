a8f8e07374bf7c41fe102e3a635b6a68b31e6f7f
Junrui Lin <linraymo@amazon.com>
2019-10-23 12:19:19 -0700

    Updating.

--- 20190821/search-in-rotated-sorted-array.java
+++ 20190821/search-in-rotated-sorted-array.java
@@ -26,6 +26,72 @@ class Solution {
             return -1;
         }
 
+        // return mytry(nums, target);
+
+        // return method2(nums, target);
+
+        return method3(nums, target);
+    }
+
+    private int method3(int[] nums, int target) {
+        int n = nums.length;
+        int start = 0;
+        int end = n - 1;
+        // find the smallest number first
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[end]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        // start & end is the index of smallest number
+        int index = start;
+        start = 0;
+        end = n - 1;
+        while (start < end) {
+            int num = start + (end - start) / 2;
+            int mid = (num + index) % n;
+            // 相当于把 rotate 那一段移到非 rotate 后面, 或是想像成 cycle
+            if (nums[mid] > target) {
+                end = num - 1;
+
+            } else {
+                start = num;
+            }
+        }
+        return nums[(start + index) % n] == target ? (start + index) % n : -1;
+    }
+
+    private int method2(int[] nums, int target) {
+        int n = nums.length;
+        int start = 0;
+        int end = n - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] >= nums[start]) {
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return nums[start] == target ? start : -1;
+        // 或者 while 判断改为 start <= end, 这里就直接返回 -1， 因为最后一次查 start 也在 while 中完成了
+    }
+
+    private int mytry(int[] nums, int target) {
         int n = nums.length;
         int start = 0;
         int end = n - 1;
