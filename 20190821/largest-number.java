1226f4674ce15663398204864fca5f8b76418721
Junrui Lin <linraymo@amazon.com>
2019-11-13 00:39:41 -0800

    Updating.

---
+++ 20190821/largest-number.java
@@ -0,0 +1,48 @@
+// 179. Largest Number
+// DescriptionHintsSubmissionsDiscussSolution
+//
+// Given a list of non negative integers, arrange them such that they form the largest number.
+//
+// Example 1:
+//
+// Input: [10,2]
+// Output: "210"
+//
+// Example 2:
+//
+// Input: [3,30,34,5,9]
+// Output: "9534330"
+//
+// Note: The result may be very large, so you need to return a string instead of an integer.
+// Seen this question in a real interview before?
+//
+//     Difficulty:Medium
+
+
+class Solution {
+    public String largestNumber(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return "";
+        }
+
+        return method1(nums);
+    }
+
+    private String method1(int[] nums) {
+        // turn int -> string to compare
+        int n = nums.length;
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(arr, (o1, o2) -> ((o2 + o1).compareTo(o1 + o2)));
+        StringBuilder sb = new StringBuilder();
+        for (String s : arr) {
+            sb.append(s);
+        }
+        if (sb.charAt(0) == '0') {
+            return "0";
+        }
+        return sb.toString();
+    }
+}
